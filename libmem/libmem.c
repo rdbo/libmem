40e39d24d8dde75f1ecef40857078ffdcf55c128
rdbo <rdbodev@gmail.com>
2021-01-31 01:13:40 -0300

    fixed issues

--- libmem/libmem.c
+++ libmem/libmem.c
@@ -1159,8 +1159,9 @@ mem_module_t       mem_ex_get_module(mem_process_t process, mem_tstring_t module
 
 
 	holder = maps_buffer;
-	for (mem_tchar_t* temp = &maps_buffer[-1]; (mem_uintptr_t)(temp = MEM_STR_STR(&temp[1], module_str)) < (mem_uintptr_t)module_end_ptr; holder = &temp[1]);
+	for (mem_tchar_t* temp = &maps_buffer[-1]; (mem_uintptr_t)(temp = MEM_STR_STR(&temp[1], module_str)) < (mem_uintptr_t)module_end_ptr; holder = temp);
 	module_end_ptr = holder;
+	module_end_ptr = &module_end_ptr[MEM_STR_LEN(module_str)];
 	module_end_ptr = MEM_STR_CHR(module_end_ptr, MEM_STR('-'));
 
 	if (!module_end_ptr)

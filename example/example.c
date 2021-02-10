e4bedb0522e6523df26a8da7a85f3b45a62f8441
rdbo <rdbodev@gmail.com>
2021-02-10 08:06:21 -0300

    fixed examples

--- example/example.c
+++ example/example.c
@@ -6,18 +6,6 @@
 #define print(...) printf(__VA_ARGS__)
 #endif
 
-#if   MEM_OS == MEM_WIN
-#define NAKEDFN __declspec(naked) void
-#elif MEM_OS == MEM_LINUX
-#define NAKEDFN void __attribute__((naked))
-#endif
-
-#if   MEM_ARCH == _MEM_ARCH_x86_32
-#define EXAMPLE_HOOK 1
-#elif MEM_ARCH == _MEM_ARCH_x86_64
-#define EXAMPLE_HOOK 0
-#endif
-
 #define tprint(...)  print(MEM_STR("    ") __VA_ARGS__)
 #define separator()  print(MEM_STR("--------------------\n"))
 #define tseparator() tprint(MEM_STR("--------------------\n"))
@@ -30,76 +18,6 @@
  *  ----------------------------------\n\
  */\n\
 \n"))
-
-#if   EXAMPLE_HOOK
-
-mem_voidptr_t HookReturn = NULL;
-
-void success_function()
-{
-	tprint(MEM_STR("Hooked Succeeded!\n"));
-}
-
-void failure_function()
-{
-	tprint(MEM_STR("Hook Failed!\n"));
-}
-
-NAKEDFN target_function(void *fn)
-{
-#	ifdef _MSC_VER
-	__asm
-	{
-		//Bytes for the hook
-		nop
-		nop
-		nop
-		nop
-		nop
-		nop
-		nop
-		nop
-		nop
-		nop
-		nop
-		nop
-		nop
-		nop
-		nop
-		nop
-
-		push ebp
-		mov ebp, esp
-		push eax
-		//Call function passed in 'fn'
-		mov eax, [ebp + 8]
-		call eax
-		pop eax
-
-		mov esp, ebp
-		pop ebp
-		ret
-	}
-#	else
-#	endif
-}
-
-NAKEDFN hook_function()
-{
-#	if   MEM_OS == MEM_WIN
-	__asm
-	{
-		push eax
-		mov eax, success_function
-		mov [ebp + 8], eax
-		pop eax
-		jmp HookReturn
-	}
-#	elif MEM_OS == MEM_LINUX
-#	endif
-}
-
-#endif //MEM_ARCH
 
 int main()
 {
@@ -181,16 +99,6 @@ int main()
 	tprint(MEM_STR("Written:         %i\n"), write_buf);
 	tprint(MEM_STR("Read:            %i\n"), read_buf);
 	tseparator();
-
-#	if   EXAMPLE_HOOK
-	tprint(MEM_STR("Target Function: %p\n"), (void *)target_function);
-	tprint(MEM_STR("Hook   Function: %p\n"), (void *)hook_function);
-	mem_byte_t *p_target_function = (mem_byte_t *)target_function;
-	if (p_target_function[0] == 0xE9) p_target_function = &p_target_function[(*(mem_intptr_t *)(&p_target_function[1])) + 5];
-	HookReturn = mem_in_detour_trampoline((mem_voidptr_t)p_target_function, (mem_voidptr_t)hook_function, mem_in_detour_size(MEM_ASM_x86_JMP32), MEM_ASM_x86_JMP32, NULL);
-	target_function((void *)failure_function);
-	tseparator();
-#	endif
 
 	//Free memory
 	free(module_path);

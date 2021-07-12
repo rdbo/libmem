8005104e6f6ab0e92412264d496e24f2c5c1d4c7
rdbo <rdbodev@gmail.com>
2021-07-12 14:12:16 -0300

    improved makefiles

--- BSD.mk
+++ BSD.mk
@@ -4,22 +4,22 @@ OUT_DIR=./bin
 LIBMEM_DIR=./libmem
 LIBMEM_SRC=$(LIBMEM_DIR)/libmem.c
 LIBMEM_OUT=libmem.so
-LIBMEM_CFLAGS=-shared -fPIC -DLM_EXPORT -ldl -lkvm -lprocstat -lelf -lutil
+LIBMEM_CFLAGS=-o $(OUT_DIR)/$(LIBMEM_OUT) -shared -fPIC -DLM_EXPORT -ldl -lkvm -lprocstat -lelf -lutil
 TESTS_DIR=./tests
 TESTS_SRC=$(TESTS_DIR)/tests.c
 TESTS_OUT=tests
-TESTS_CFLAGS=-I$(LIBMEM_DIR) -DLM_IMPORT -DTARGET_NAME=\"$(TESTS_OUT)\" -Wl,-R,$(OUT_DIR) -Wl,--enable-new-dtags -L$(OUT_DIR) -l:$(LIBMEM_OUT)
+TESTS_CFLAGS=-o $(OUT_DIR)/$(TESTS_OUT) -I$(LIBMEM_DIR) -DLM_IMPORT -DTARGET_NAME=\"$(TESTS_OUT)\" -Wl,-R,$(OUT_DIR) -Wl,--enable-new-dtags -L$(OUT_DIR) -l:$(LIBMEM_OUT)
 
 all: setup libmem tests
 
 libmem: setup
 	@echo "[+] Building 'libmem'"
-	$(CC) -o $(OUT_DIR)/$(LIBMEM_OUT) $(CFLAGS) $(LIBMEM_CFLAGS) $(LIBMEM_SRC)
+	$(CC) $(CFLAGS) $(LIBMEM_CFLAGS) $(LIBMEM_SRC)
 	@echo "[-] Done building 'libmem'"
 
 tests: setup
 	@echo "[+] Building 'tests'"
-	$(CC) -o $(OUT_DIR)/$(TESTS_OUT) $(CFLAGS) $(TESTS_CFLAGS) $(TESTS_SRC)
+	$(CC) $(CFLAGS) $(TESTS_CFLAGS) $(TESTS_SRC)
 	@echo "[-] Done building 'tests'"
 
 setup:
--- Win.mk
+++ Win.mk
@@ -1,2 +1,29 @@
-all:
-	@echo "[*] WIP"
+CC=cl.exe
+CFLAGS=/Z7
+OUT_DIR=.\bin
+LIBMEM_DIR=.\libmem
+LIBMEM_SRC=$(LIBMEM_DIR)\libmem.c
+LIBMEM_OUT=libmem.dll
+LIBMEM_CFLAGS=/Fo $(OUT_DIR)\libmem.o /LD /MD /D LM_EXPORT /link /DLL /IMPLIB:"$(OUT_DIR)\libmem.lib" /OUT:"$(OUT_DIR)\$(LIBMEM_OUT)"
+TESTS_DIR=.\tests
+TESTS_SRC=$(TESTS_DIR)\tests.c
+TESTS_OUT=tests
+TESTS_CFLAGS=/link /OUT:"$(OUT_DIR)\$(TESTS_OUT)"
+
+all: setup libmem tests
+
+libmem: setup
+	@echo "[+] Building 'libmem'"
+	$(CC) $(CFLAGS) $(LIBMEM_CFLAGS) $(LIBMEM_SRC)
+	@echo "[-] Done building 'libmem'"
+
+tests: setup
+	@echo "[+] Building 'tests'"
+	$(CC) $(CFLAGS) $(TESTS_CFLAGS) $(TESTS_SRC)
+	@echo "[-] Done building 'tests'"
+
+setup:
+	@if not exist "$(OUT_DIR)" (mkdir "$(OUT_DIR)")
+
+clean:
+	@if not exist "$(OUT_DIR)" (rmdir /Q /S "$(OUT_DIR)")

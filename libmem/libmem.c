6294c81d375e4077fa4a964d700cf537a0e290cf
rdbo <rdbodev@gmail.com>
2021-07-25 16:15:38 -0300

    added trampoline functions

--- libmem/libmem.c
+++ libmem/libmem.c
@@ -3220,19 +3220,107 @@ LM_DetourCodeEx(lm_process_t proc,
 LM_API lm_address_t
 LM_MakeTrampoline(lm_address_t src,
 		  lm_address_t dst,
-		  lm_size_t    size);
+		  lm_size_t    size)
+{
+	lm_address_t tramp = (lm_address_t)LM_BAD;
+	lm_prot_t    old_prot = LM_PROT_XRW;
+
+	if (!LM_ProtMemory(src, size, LM_PROT_XRW, &old_prot))
+		return tramp;
+
+#	if LM_ARCH == LM_ARCH_X86
+	{
+		lm_byte_t *payload = (lm_byte_t *)LM_NULL;
+		lm_size_t  payload_size;
+		
+		payload_size = _LM_DetourPayload(LM_NULLPTR,
+						 dst,
+						 LM_DETOUR_JMP64,
+						 LM_GetProcessBits(),
+						 &payload);
+		
+		if (!payload_size || !payload)
+			return tramp;
+
+		tramp = LM_AllocMemory(size + payload_size, LM_PROT_XRW);
+		if (!tramp)
+			goto _FREE_PAYLOAD;
+		
+		LM_WriteMemory(tramp, src, size);
+		LM_WriteMemory((lm_address_t)(&((lm_byte_t *)tramp)[size]),
+			       payload,
+			       payload_size);
+	_FREE_PAYLOAD:
+		LM_FREE(payload);
+	}
+#	elif LM_ARCH == LM_ARCH_ARM
+#	endif
+
+	LM_ProtMemory(src, size, old_prot, LM_NULLPTR);
+
+	return tramp;
+}
 
 LM_API lm_address_t
 LM_MakeTrampolineEx(lm_process_t proc,
 		    lm_address_t src,
 		    lm_address_t dst,
-		    lm_size_t    size);
+		    lm_size_t    size)
+{
+	lm_address_t tramp = (lm_address_t)LM_BAD;
+	lm_prot_t    old_prot = LM_PROT_XRW;
+
+	if (!LM_ProtMemoryEx(proc, src, size, LM_PROT_XRW, &old_prot))
+		return tramp;
+
+#	if LM_ARCH == LM_ARCH_X86
+	{
+		lm_byte_t *payload = (lm_byte_t *)LM_NULL;
+		lm_size_t  payload_size;
+		
+		payload_size = _LM_DetourPayload(LM_NULLPTR,
+						 dst,
+						 LM_DETOUR_JMP64,
+						 LM_GetProcessBits(),
+						 &payload);
+		
+		if (!payload_size || !payload)
+			return tramp;
+
+		tramp = LM_AllocMemoryEx(proc, size + payload_size,
+					 LM_PROT_XRW);
+		if (!tramp)
+			goto _FREE_PAYLOAD;
+		
+		LM_WriteMemoryEx(proc, tramp, src, size);
+		LM_WriteMemoryEx(proc,
+				 (lm_address_t)(&((lm_byte_t *)tramp)[size]),
+				 payload,
+				 payload_size);
+	_FREE_PAYLOAD:
+		LM_FREE(payload);
+	}
+#	elif LM_ARCH == LM_ARCH_ARM
+#	endif
+
+	LM_ProtMemoryEx(proc, src, size, old_prot, LM_NULLPTR);
+
+	return tramp;
+}
 
 LM_API lm_void_t
-LM_DestroyTrampoline(lm_address_t tramp);
+LM_DestroyTrampoline(lm_address_t tramp)
+{
+	if (tramp)
+		LM_FreeMemory(tramp, 0);
+}
 
 LM_API lm_void_t
 LM_DestroyTrampolineEx(lm_process_t proc,
-		       lm_address_t tramp);
+		       lm_address_t tramp)
+{
+	if (tramp)
+		LM_FreeMemoryEx(proc, tramp, 0);
+}
 
 #endif

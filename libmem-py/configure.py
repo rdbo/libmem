e0de17ed1d19ad4314b5c8e5447690f124df4edc
rdbo <rdbodev@gmail.com>
2021-08-23 09:22:39 -0300

    updated python configuration script

--- libmem-py/configure.py
+++ libmem-py/configure.py
@@ -1,21 +1,39 @@
 import os
 import shutil
 
+root_dir = os.pardir
+libmem_dir = f"{root_dir}{os.sep}libmem"
 project_dir = os.curdir
 project_src_dir = f"{project_dir}{os.sep}src/libmem-py"
 
-project_files = [
-	f"{os.pardir}{os.sep}README.md",
-	f"{os.pardir}{os.sep}LOGO.png",
-	f"{os.pardir}{os.sep}LICENSE"
-]
-project_src_files = [
-	f"{os.pardir}{os.sep}libmem{os.sep}libmem.h",
-	f"{os.pardir}{os.sep}libmem{os.sep}libmem.c"
-]
+print("[+] Configuring files...")
 
-for f in project_files:
-	shutil.copy(f, project_dir)
+project_files = {
+	# (src_dir : dst_dir) [ files ]
+	(root_dir, project_dir) : [
+		"README.md",
+		"LOGO.png",
+		"LICENSE"
+	],
 
-for f in project_src_files:
-	shutil.copy(f, project_src_dir)
+	(libmem_dir, project_src_dir) : [
+		"libmem.h",
+		"libmem.c"
+	]
+}
+
+for i in project_files:
+	src_dir = i[0]
+	dst_dir = i[1]
+	files = project_files[i]
+
+	print(f"[*] Source Directory: {src_dir}")
+	print(f"[*] Destination Directory: {dst_dir}")
+	print(f"[*] Files: {files}")
+
+	for f in files:
+		shutil.copy(f"{src_dir}{os.sep}{f}", dst_dir)
+
+	print("====================")
+
+print("[-] Configuration complete")

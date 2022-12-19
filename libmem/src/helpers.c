50bff89e89404b984c3c4549c464c3736c756562
rdbo <rdbodev@gmail.com>
2022-12-18 23:05:49 -0300

    improved performance of _LM_OpenFileBuf

--- libmem/src/helpers.c
+++ libmem/src/helpers.c
@@ -5,26 +5,27 @@ _LM_OpenFileBuf(lm_tstring_t path,
 {
 	int         fd;
 	lm_size_t   total = 0;
-	lm_tchar_t  buf[1024];
-	ssize_t     rdsize;
-	lm_tchar_t *filebuf = (lm_tchar_t *)LM_NULL;
+	ssize_t     rdsize = 0;
+	size_t      rdcount = 1024;
+	lm_tchar_t *filebuf = (lm_tchar_t *)LM_NULLPTR;
+	lm_size_t   allocsize = 1024 + sizeof(lm_tchar_t);
 
 	fd = open(path, O_RDONLY);
 	if (fd == -1)
 		return total;
-	
-	while ((rdsize = read(fd, buf, sizeof(buf))) > 0) {
+
+	do {
 		/* Use 'realloc' to increase the buffer size and copy the old data */
-		filebuf = (lm_tchar_t *)LM_REALLOC(filebuf, total + rdsize + sizeof(lm_tchar_t));
+		filebuf = (lm_tchar_t *)LM_REALLOC(filebuf, allocsize);
 
 		if (!filebuf) {
 			total = 0;
 			break;
 		}
-		
-		LM_MEMCPY(&filebuf[total], buf, rdsize);
+
 		total += rdsize;
-	}
+		allocsize += rdcount;
+	} while ((rdsize = read(fd, &((char *)filebuf)[total], rdcount)) > 0);
 
 	if (filebuf) {
 		filebuf[total] = LM_STR('\x00');

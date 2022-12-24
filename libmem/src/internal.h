9e50874528f64bb75351a51bb6f5b04c7444cd1e
rdbo <rdbodev@gmail.com>
2022-12-24 07:39:59 -0300

    updated internal.h

--- libmem/src/internal.h
+++ libmem/src/internal.h
@@ -3,28 +3,6 @@
 
 #include <libmem.h>
 
-#if LM_LANG == LM_LANG_CPP
-#define LM_PRIVATE extern "C"
-#else
-#define LM_PRIVATE
-#endif
-
-#if LM_OS == LM_OS_WIN
-LM_PRIVATE lm_bool_t
-_LM_EnumPeSyms(lm_size_t    bits,
-	       lm_address_t modbase,
-	       lm_bool_t  (*callback)(lm_cstring_t symbol,
-				      lm_address_t addr,
-				      lm_void_t   *arg),
-	       lm_void_t *arg);
-#else
-LM_PRIVATE lm_bool_t
-_LM_EnumElfSyms(lm_module_t mod,
-		lm_tchar_t *modpath,
-		lm_bool_t (*callback)(lm_cstring_t symbol,
-				      lm_address_t addr,
-				      lm_void_t   *arg),
-		lm_void_t  *arg);
-#endif
+#define LM_PRIVATE static
 
 #endif

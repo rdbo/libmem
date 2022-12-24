373b9910cb66507aafdcd6c09d0a5f3cd9231cf0
rdbo <rdbodev@gmail.com>
2022-12-23 22:28:12 -0300

    declared internal APIs

--- libmem/src/internal.h
+++ libmem/src/internal.h
@@ -10,9 +10,24 @@
 #endif
 
 #if LM_OS == LM_OS_WIN
+LM_PRIVATE lm_bool_t
+_LM_EnumPeSyms(lm_size_t    bits,
+	       lm_address_t modbase,
+	       lm_bool_t  (*callback)(lm_cstring_t symbol,
+				      lm_address_t addr,
+				      lm_void_t   *arg),
+	       lm_void_t *arg);
 #else
 LM_PRIVATE lm_size_t
 _LM_GetElfBits(lm_tchar_t *path);
+
+LM_PRIVATE lm_bool_t
+_LM_EnumElfSyms(lm_module_t mod,
+		lm_tchar_t *modpath,
+		lm_bool_t (*callback)(lm_cstring_t symbol,
+				      lm_address_t addr,
+				      lm_void_t   *arg),
+		lm_void_t  *arg);
 #endif
 
 #endif

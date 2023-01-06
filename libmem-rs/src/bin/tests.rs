5a98267c766d7b5e2474dff3af5fb795cee6a398
rdbo <rdbodev@gmail.com>
2023-01-05 21:56:56 -0300

    added process enumeration for Rust API

--- libmem-rs/src/bin/tests.rs
+++ libmem-rs/src/bin/tests.rs
@@ -8,4 +8,8 @@ fn main() {
     println!("Process Bits: {}", process.get_bits());
     println!("Process Path: {}", process.get_path());
     println!("Process Name: {}", process.get_name());
+
+    for proc in LM_EnumProcesses() {
+        println!("{} | {} | {} | {} | {}", proc.get_pid(), proc.get_ppid(), proc.get_bits(), proc.get_path(), proc.get_name());
+    }
 }
--- libmem-rs/src/lib.rs
+++ libmem-rs/src/lib.rs
@@ -4,9 +4,13 @@
 
 /* Note: the types and structures must be
  * the same size and aligned with their C variations */
+const LM_FALSE : i32 = 0;
+const LM_TRUE : i32 = 1;
 const LM_PATH_MAX : usize = 512;
 
 #[repr(C)]
+#[derive(Clone)]
+#[derive(Copy)]
 pub struct lm_process_t {
     pid : u32,
     ppid : u32,
@@ -45,19 +49,45 @@ mod libmem_c {
     // link against 'mem' (the lib prefix is appended automatically)
     #[link(name = "mem")]
     extern "C" {
+        pub(super) fn LM_EnumProcesses(callback : extern "C" fn(*const lm_process_t, *mut ()) -> i32, arg : *mut ()) -> i32;
         pub(super) fn LM_GetProcess(pproc : *mut lm_process_t) -> i32;
     }
 }
 
 // Rustified libmem calls
-pub fn LM_GetProcess() -> Option<lm_process_t> {
-    let mut proc = lm_process_t { pid: 0, ppid: 0, bits: 0, path: [0;LM_PATH_MAX], name: [0;LM_PATH_MAX] };
+extern "C" fn _LM_EnumProcessesCallback(pproc : *const lm_process_t, arg : *mut ()) -> i32 {
+    let proc_list_ptr = arg as *mut Vec<lm_process_t>;
     unsafe {
-        if libmem_c::LM_GetProcess(&mut proc as *mut lm_process_t) != 0 {
-            return Some(proc);
+        (*proc_list_ptr).push(*pproc);
+    }
+    LM_TRUE
+}
+
+pub fn LM_EnumProcesses() -> Vec<lm_process_t> {
+    let mut proc_list : Vec<lm_process_t> = Vec::new();
+    unsafe {
+        let arg = &mut proc_list as *mut Vec<lm_process_t>;
+        let arg = arg as *mut ();
+        if libmem_c::LM_EnumProcesses(_LM_EnumProcessesCallback, arg) == LM_FALSE {
+            proc_list.clear();
         }
     }
 
-    return None;
+    proc_list
+}
+
+pub fn LM_GetProcess() -> Option<lm_process_t> {
+    let mut proc = lm_process_t {
+        pid: 0, ppid: 0, bits: 0,
+        path: [0;LM_PATH_MAX], name: [0;LM_PATH_MAX]
+    };
+
+    unsafe {
+        if libmem_c::LM_GetProcess(&mut proc as *mut lm_process_t) != LM_FALSE {
+            Some(proc)
+        } else {
+            None
+        }
+    }
 }
 

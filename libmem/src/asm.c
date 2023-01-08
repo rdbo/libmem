35d2f239ca14c8a8fedde904adbd18b75185ed8e
rdbo <rdbodev@gmail.com>
2023-01-07 23:31:30 -0300

    fixed bug in LM_CodeLength

--- libmem/src/asm.c
+++ libmem/src/asm.c
@@ -207,7 +207,7 @@ LM_CodeLength(lm_address_t code,
 
 	LM_ASSERT(code != LM_ADDRESS_BAD && minlength > 0);
 
-	for (length = 0; length < minlength; code = (lm_address_t)LM_OFFSET(code, length)) {
+	for (length = 0; length < minlength; code = (lm_address_t)LM_OFFSET(code, inst.size)) {
 		if (LM_Disassemble(code, &inst) == LM_FALSE)
 			return 0;
 		length += inst.size;

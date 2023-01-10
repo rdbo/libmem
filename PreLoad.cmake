a70c3df157bb981a211af9f6b23fca5d3415aab1
rdbo <rdbodev@gmail.com>
2023-01-10 10:34:12 -0300

    fixed PreLoad.cmake for Windows

--- PreLoad.cmake
+++ PreLoad.cmake
@@ -1,4 +1,4 @@
-if(${CMAKE_SYSTEM_NAME} MATCHES Windows)
+if(WIN32)
 	set(CMAKE_GENERATOR "NMake Makefiles" CACHE INTERNAL "" FORCE)
 else()
 	set(CMAKE_GENERATOR "Unix Makefiles" CACHE INTERNAL "" FORCE)

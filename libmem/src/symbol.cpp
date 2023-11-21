a085b928221944ed05fabd59cc15b7f4ca9b53c3
rdbo <rdbodev@gmail.com>
2023-11-21 11:17:49 -0300

    fixed keyword on symbol

--- libmem/src/symbol.cpp
+++ libmem/src/symbol.cpp
@@ -44,11 +44,11 @@ _LM_EnumPeSyms(lm_module_t *pmod,
 
 	binary = Parser::parse(pmod->path);
 
-	auto export = binary->get_export();
-	if (!export)
+	auto ex = binary->get_export();
+	if (!ex)
 		return LM_FALSE;
 
-	for (const ExportEntry &symbol : export->entries()) {
+	for (const ExportEntry &symbol : ex->entries()) {
 		sym.name = (lm_cstring_t)symbol.name().c_str();
 		sym.address = (lm_address_t)LM_OFFSET(pmod->base, symbol.value());
 		if (!callback(&sym, arg))

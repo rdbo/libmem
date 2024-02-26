61cf215ac2576251729b513e82f429b9d34b714b
Cherry <13651622+MolotovCherry@users.noreply.github.com>
2024-02-25 19:41:03 -0800

    Allow customizing lib search path for Rust when building on Windows (#186)
    * Allow customizing lib search path for libmem on Rust when building for Windows
    
    * Add additional path instead of replacing old one
    
    * Changed variable name
    
    ---------
    
    Co-authored-by: Rdbo <57117082+rdbo@users.noreply.github.com>

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -2,6 +2,10 @@ use std::env;
 
 fn main() {
     if cfg!(windows) {
+        if let Ok(path) = env::var("LIBMEM_DIR") {
+            println!("cargo:rustc-link-search={}", path);
+        }
+
         let lib_path = format!("{}{}", env::var("ProgramFiles").unwrap(), "\\libmem\\lib");
         println!("cargo:rustc-link-search={}", lib_path);
     };

b8d0d556f5019f3ac46a8be410c8f7ef3257738f
rdbo <rdbodev@gmail.com>
2024-02-29 09:36:37 -0300

    moved freebsd utils to separate files

---
+++ src/freebsd/utils.h
@@ -0,0 +1,31 @@
+/*
+ *  ----------------------------------
+ * |         libmem - by rdbo         |
+ * |      Memory Hacking Library      |
+ *  ----------------------------------
+ */
+
+/*
+ * Copyright (C) 2023    Rdbo
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License version 3
+ * as published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <libmem/libmem.h>
+
+lm_time_t
+get_process_start_time(struct kinfo_proc *proc);
+
+#endif

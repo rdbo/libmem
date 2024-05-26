f467047962a853e59377cd3b172daa16192e5966
rdbo <rdbodev@gmail.com>
2024-05-26 19:15:38 -0300

    implemented segment api for rust

--- bindings/rust/libmem/src/segment.rs
+++ bindings/rust/libmem/src/segment.rs
@@ -1,7 +1,7 @@
-use libmem_sys::{lm_bool_t, lm_segment_t, lm_void_t, LM_TRUE};
+use libmem_sys::{lm_bool_t, lm_process_t, lm_segment_t, lm_void_t, LM_TRUE};
 use std::{fmt, mem::MaybeUninit};
 
-use crate::{Address, Prot};
+use crate::{Address, Process, Prot};
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Segment {
@@ -54,10 +54,40 @@ pub fn enum_segments() -> Option<Vec<Segment>> {
     (result == LM_TRUE).then_some(segments)
 }
 
+/// Enumerates the consecutive memory segments in the current process
+pub fn enum_segments_ex(process: &Process) -> Option<Vec<Segment>> {
+    let raw_process: lm_process_t = process.to_owned().into();
+    let mut segments: Vec<Segment> = Vec::new();
+    let result = unsafe {
+        libmem_sys::LM_EnumSegmentsEx(
+            &raw_process as *const lm_process_t,
+            enum_segments_callback,
+            &mut segments as *mut Vec<Segment> as *mut lm_void_t,
+        )
+    };
+
+    (result == LM_TRUE).then_some(segments)
+}
+
 /// Finds the segment where a specific memory address is located in
 pub fn find_segment(address: Address) -> Option<Segment> {
     let mut segment: MaybeUninit<lm_segment_t> = MaybeUninit::uninit();
     let result = unsafe { libmem_sys::LM_FindSegment(address, segment.as_mut_ptr()) };
+
+    (result == LM_TRUE).then_some(unsafe { segment.assume_init() }.into())
+}
+
+/// Finds the segment where a specific memory address is located in
+pub fn find_segment_ex(process: &Process, address: Address) -> Option<Segment> {
+    let raw_process: lm_process_t = process.to_owned().into();
+    let mut segment: MaybeUninit<lm_segment_t> = MaybeUninit::uninit();
+    let result = unsafe {
+        libmem_sys::LM_FindSegmentEx(
+            &raw_process as *const lm_process_t,
+            address,
+            segment.as_mut_ptr(),
+        )
+    };
 
     (result == LM_TRUE).then_some(unsafe { segment.assume_init() }.into())
 }

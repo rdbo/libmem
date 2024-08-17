3483e4e2387ef4e4e3cc7856f8021bbda263943c
Saikari <lin@sz.cn.eu.org>
2024-08-17 23:27:51 +0300

    Merge Dev (#250)
    * Update build.rs
    
    Fixed x86 lib fetch for Rust

--- bindings/rust/libmem-sys/build.rs
+++ bindings/rust/libmem-sys/build.rs
@@ -18,7 +18,11 @@ fn download_and_resolve_libmem() {
     let version = env::var("CARGO_PKG_VERSION").unwrap();
     let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
     let os_name = env::var("CARGO_CFG_TARGET_OS").unwrap();
-    let arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    let mut arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    
+    if arch == "x86" {
+        arch = "i686".to_string();
+    }
     let target_env = if target_os == "linux" && cfg!(feature = "static") {
         // Always use musl for static linking on Linux
         "musl".to_owned()
